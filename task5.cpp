c6378d033bf1bd9b12ca92508c53f6ca35a774ee
Aditya Lawankar <70308865+aditya-lawankar@users.noreply.github.com>
2023-02-16 23:14:00 +0530

    Create task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Updated C++ file.\n";
+  cout << "At task 5.\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
